5a1b3707790d144d0bd70891a00d99681dfab03b
Tim Qi <you@example.com>
2024-06-09 01:41:17 +0100

    ADD: main - working snapshotListener

--- app/src/main/java/com/example/firebasetest/MainActivity.java
+++ app/src/main/java/com/example/firebasetest/MainActivity.java
@@ -1,24 +1,56 @@
 package com.example.firebasetest;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
 public class MainActivity extends AppCompatActivity {
+
+    // creating a variable for text view.
+    TextView updatedTV;
+
+    // initializing the variable for firebase firestore
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        updatedTV = findViewById(R.id.idTVUpdate);
+
+        DocumentReference docRef = db.collection("MyData").document("Data");
+
+        docRef.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+           @Override
+           public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+               if (error != null) {
+                   Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                   return;
+               }
+               if (value != null && value.exists()) {
+                   updatedTV.setText(value.getData().get("updatedValue").toString());
+               }
+           }
+       });
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
     }
 }
